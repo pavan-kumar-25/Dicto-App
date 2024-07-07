0a70f5da2ac267e3013c8b1e55bb578bf66adcb6
pavan-kumar-25 <pavankumar.c@mca.christuniversity.in>
2024-07-07 12:56:15 +0530

    new

---
+++ src/app/components/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% h-[30vh]">
+      <h1>Banner</h1>
+    </div>
+  )
+}
+
+export default Banner
